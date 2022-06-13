9e43396f237ef19fe2727d2f1f871657f2d8845d
gravy-jones <gravy@gravyjoneslocker.uk>
2022-06-13 14:28:36 +0100

    Version 3.0 + .whl

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     install_requires=[
         'requests',
         'selenium-wire',
+        'selenium',
         'packaging',
         'lxml',
         'undetected-chromedriver>=3.1.5r4',
